e8269bcac14c16815208ab8a2adc474fccd62c2d
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-06-04 18:54:30 -0500

    Add default mRLFE complex-k parameters

--- models/mrlfe/defaultMRLFEParams.m
+++ models/mrlfe/defaultMRLFEParams.m
@@ -1,16 +1,17 @@
 function mrlfeParams = defaultMRLFEParams()
-% Return default parameters for the mRLFE elastic plotting prototype.
-%
-% This first implementation solves a real-k elastic version of the modified
-% Rayleigh-Lamb fluid-loaded model. Viscosity and complex-k attenuation are
-% intentionally reserved for a later implementation stage.
+% Return default parameters for mRLFE model variants.
 
 mrlfeParams = struct();
 mrlfeParams.fluidDensity = 1000;       % [kg/m^3]
 mrlfeParams.fluidSoundSpeed = 1500;    % [m/s]
-mrlfeParams.etaL = 0;                  % [Pa*s], reserved for future complex model
-mrlfeParams.etaS = 0;                  % [Pa*s], reserved for future complex model
+mrlfeParams.etaL = 0;                  % [Pa*s]
+mrlfeParams.etaS = 0;                  % [Pa*s]
 mrlfeParams.solveComplexK = false;
 mrlfeParams.seedSource = "RayleighLamb";
 mrlfeParams.branchNames = ["A0Like", "S0Like"];
+
+% Complex-k prototype controls.
+mrlfeParams.initialImagKFraction = 1e-5;
+mrlfeParams.maxImagKFraction = 0.50;
+mrlfeParams.minImagKAbsolute = 1e-9;
 end
