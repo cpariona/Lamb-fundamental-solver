a6e48099880466783e99caeb9d0bd7a07665f24d
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-06-16 18:22:58 -0500

    Remove redundant failure landscape short alias

--- examples/acoustoelastic_iop_hgo/diagnostics/diagnose_failure_landscape_short.m
+++
@@ -1,8 +0,0 @@
-clear; clc; close all;
-
-%DIAGNOSE_FAILURE_LANDSCAPE_SHORT Short AE IOP/HGO failure-landscape diagnostic entrypoint.
-
-thisFile = mfilename('fullpath');
-thisFolder = fileparts(thisFile);
-scriptPath = fullfile(thisFolder, 'diagnose_acoustoelastic_iop_hgo_failure_landscape.m');
-aeRunLegacyScript(scriptPath);
