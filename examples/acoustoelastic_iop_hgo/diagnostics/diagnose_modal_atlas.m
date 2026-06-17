68c967c20894616ab1f18a1bc33eba35f5a9f0e1
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-06-16 20:38:28 -0500

    Copy modal atlas legacy outputs to short path

--- examples/acoustoelastic_iop_hgo/diagnostics/diagnose_modal_atlas.m
+++ examples/acoustoelastic_iop_hgo/diagnostics/diagnose_modal_atlas.m
@@ -2,7 +2,13 @@ clear; clc; close all;
 
 %DIAGNOSE_MODAL_ATLAS Short AE IOP/HGO modal-atlas diagnostic entrypoint.
 
+launchFolder = pwd;
 thisFile = mfilename('fullpath');
 thisFolder = fileparts(thisFile);
 scriptPath = fullfile(thisFolder, 'diagnose_acoustoelastic_iop_hgo_modal_atlas.m');
 aeRunLegacyScript(scriptPath);
+aeCopyLegacyResultFolder(launchFolder, ...
+    'acoustoelastic_iop_hgo_modal_atlas', ...
+    'modal_atlas', ...
+    'acoustoelastic_iop_hgo_modal_atlas', ...
+    'modal_atlas');
