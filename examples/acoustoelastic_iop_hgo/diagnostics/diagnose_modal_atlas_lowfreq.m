99d598b175870a43e01962bc931ba85b19306ecd
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-06-16 20:38:14 -0500

    Copy low-frequency modal atlas legacy outputs to short path

--- examples/acoustoelastic_iop_hgo/diagnostics/diagnose_modal_atlas_lowfreq.m
+++ examples/acoustoelastic_iop_hgo/diagnostics/diagnose_modal_atlas_lowfreq.m
@@ -2,7 +2,13 @@ clear; clc; close all;
 
 %DIAGNOSE_MODAL_ATLAS_LOWFREQ Short AE IOP/HGO low-frequency modal-atlas diagnostic entrypoint.
 
+launchFolder = pwd;
 thisFile = mfilename('fullpath');
 thisFolder = fileparts(thisFile);
 scriptPath = fullfile(thisFolder, 'diagnose_acoustoelastic_iop_hgo_low_frequency_modal_atlas.m');
 aeRunLegacyScript(scriptPath);
+aeCopyLegacyResultFolder(launchFolder, ...
+    'acoustoelastic_iop_hgo_low_frequency_modal_atlas', ...
+    'modal_atlas_lowfreq', ...
+    'acoustoelastic_iop_hgo_low_frequency_modal_atlas', ...
+    'modal_atlas_lowfreq');
