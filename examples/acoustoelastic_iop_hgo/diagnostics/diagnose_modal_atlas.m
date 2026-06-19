786deb4250f88477db94fc6645a6e59fee7748ad
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-06-19 14:01:54 -0500

    Route modal atlas to short output folder

--- examples/acoustoelastic_iop_hgo/diagnostics/diagnose_modal_atlas.m
+++ examples/acoustoelastic_iop_hgo/diagnostics/diagnose_modal_atlas.m
@@ -1,14 +1,33 @@
 clear; clc; close all;
 
 %DIAGNOSE_MODAL_ATLAS Short AE IOP/HGO modal-atlas diagnostic entrypoint.
+%
+% Executes a temporary copy of the legacy implementation with the output folder
+% redirected to Results/ae_iop_hgo/modal_atlas.
 
 launchFolder = pwd;
 thisFile = mfilename('fullpath');
 thisFolder = fileparts(thisFile);
-scriptPath = fullfile(thisFolder, 'diagnose_acoustoelastic_iop_hgo_modal_atlas.m');
-aeRunLegacyScript(scriptPath);
-aeCopyLegacyResultFolder(launchFolder, ...
-    'acoustoelastic_iop_hgo_modal_atlas', ...
-    'modal_atlas', ...
-    'acoustoelastic_iop_hgo_modal_atlas', ...
-    'modal_atlas');
+legacyPath = fullfile(thisFolder, 'diagnose_acoustoelastic_iop_hgo_modal_atlas.m');
+
+sourceText = fileread(legacyPath);
+oldLine = "outputFolder = fullfile(pwd, 'Results', 'acoustoelastic_iop_hgo_modal_atlas');";
+newLine = "outputFolder = aeOutputFolder(launchFolder, 'modal_atlas');";
+if ~contains(sourceText, oldLine)
+    error('Expected modal-atlas output-folder line not found.');
+end
+sourceText = strrep(sourceText, oldLine, newLine);
+
+shortScript = fullfile(tempdir, 'ae_iop_hgo_modal_atlas_short_output.m');
+writeTemporaryScript(shortScript, launchFolder, sourceText);
+run(shortScript);
+
+function writeTemporaryScript(fileName, launchFolder, sourceText)
+fid = fopen(fileName, 'w');
+if fid < 0
+    error('Could not create temporary modal-atlas script: %s', fileName);
+end
+cleanupObj = onCleanup(@() fclose(fid)); %#ok<NASGU>
+launchFolderEscaped = strrep(launchFolder, '''', '''''');
+fprintf(fid, 'cd(''%s'');\nlaunchFolder = ''%s'';\n%s', launchFolderEscaped, launchFolderEscaped, sourceText);
+end
