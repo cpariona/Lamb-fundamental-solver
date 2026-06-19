f8b67975f153bd72eb730bc785bc507befd3bb0f
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-06-19 18:39:59 -0500

    Remove legacy resolution wrapper

--- examples/acoustoelastic_iop_hgo/diagnostics/diagnose_acoustoelastic_iop_hgo_atlasA0_resolution_sensitivity.m
+++
@@ -1,6 +0,0 @@
-clear; clc; close all;
-
-%DIAGNOSE_ACOUSTOELASTIC_IOP_HGO_ATLASA0_RESOLUTION_SENSITIVITY Legacy compatibility alias.
-% Prefer diagnose_atlas_resolution for new calls.
-
-diagnose_atlas_resolution;
