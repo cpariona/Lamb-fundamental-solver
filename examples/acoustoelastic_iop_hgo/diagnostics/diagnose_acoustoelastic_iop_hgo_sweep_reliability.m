953efb3a5fb39704a6caa295f39653856ef21389
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-06-19 20:01:00 -0500

    Remove legacy sweep reliability alias

--- examples/acoustoelastic_iop_hgo/diagnostics/diagnose_acoustoelastic_iop_hgo_sweep_reliability.m
+++
@@ -1,6 +0,0 @@
-clear; clc; close all;
-
-%DIAGNOSE_ACOUSTOELASTIC_IOP_HGO_SWEEP_RELIABILITY Legacy compatibility alias.
-% Prefer diagnose_sweep_reliability for new calls.
-
-diagnose_sweep_reliability;
