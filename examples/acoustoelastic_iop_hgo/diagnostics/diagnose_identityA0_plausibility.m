1070ae31efffd53e20dcbe2c9314075da620cd50
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-06-19 20:04:47 -0500

    Remove identityA0 plausibility alias

--- examples/acoustoelastic_iop_hgo/diagnostics/diagnose_identityA0_plausibility.m
+++
@@ -1,9 +0,0 @@
-clear; clc; close all;
-
-%DIAGNOSE_IDENTITYA0_PLAUSIBILITY Short MATLAB-compatible entrypoint.
-% Prefer diagnose_idA0_plausibility for new calls.
-
-thisFile = mfilename('fullpath');
-thisFolder = fileparts(thisFile);
-legacyScript = fullfile(thisFolder, 'diagnose_idA0_plausibility_impl.m');
-aeRunLegacyScript(legacyScript);
