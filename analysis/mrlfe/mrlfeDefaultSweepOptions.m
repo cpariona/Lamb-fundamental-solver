950a1b30ff3f4b34c6964d262ef64242aad92d3e
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-06-21 18:32:13 -0500

    Add mRLFE sweep options helper

---
+++ analysis/mrlfe/mrlfeDefaultSweepOptions.m
@@ -0,0 +1,37 @@
+function options = mrlfeDefaultSweepOptions(branchName, varargin)
+%MRLFEDEFAULTSWEEPOPTIONS Build reference solver options for mRLFE sweeps.
+%
+% branchName must be "A0Like" or "S0Like". The helper configures the
+% corresponding Rayleigh-Lamb seed branch and mRLFE branch flag.
+
+p = inputParser;
+addRequired(p, 'branchName', @(x)ischar(x) || isstring(x));
+addParameter(p, 'EtaS', 0.05, @(x)isnumeric(x) && isscalar(x) && isfinite(x));
+parse(p, branchName, varargin{:});
+
+branchName = string(p.Results.branchName);
+
+options = rlDefaultOptions("Fast");
+options.computeMRLFERealK = true;
+options.computeMRLFEHanViscoRealK = true;
+options.computeMRLFEComplexK = false;
+options.mrlfeParams = defaultMRLFEParams();
+options.mrlfeParams.fluidDensity = 1000;
+options.mrlfeParams.fluidSoundSpeed = 1500;
+options.mrlfeParams.etaS = p.Results.EtaS;
+
+switch branchName
+    case "A0Like"
+        options.computeA0 = true;
+        options.computeS0 = false;
+        options.mrlfeComputeA0Like = true;
+        options.mrlfeComputeS0Like = false;
+    case "S0Like"
+        options.computeA0 = false;
+        options.computeS0 = true;
+        options.mrlfeComputeA0Like = false;
+        options.mrlfeComputeS0Like = true;
+    otherwise
+        error('Unsupported mRLFE branchName "%s". Use "A0Like" or "S0Like".', branchName);
+end
+end
