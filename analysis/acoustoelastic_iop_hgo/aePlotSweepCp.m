7a6e3a6ae157dac5eee912ca74e202cd41aa9afc
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-06-21 17:41:17 -0500

    Use padded sweep limits for AE Cp plots

--- analysis/acoustoelastic_iop_hgo/aePlotSweepCp.m
+++ analysis/acoustoelastic_iop_hgo/aePlotSweepCp.m
@@ -33,7 +33,7 @@ end
 xlabel(ax, 'Frequency [kHz]');
 ylabel(ax, 'Phase velocity Cp [m/s]');
 grid(ax, 'on');
-setAxesOriginLimits(ax);
+setSweepPlotLimits(ax, 'CpAxis', 'y');
 legend(ax, 'Location', 'best');
 
 if strlength(string(p.Results.Title)) > 0
