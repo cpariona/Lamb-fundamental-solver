70d7a95c89f2072bfcc7f50b6d9b7d3002e9c9d8
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-06-21 19:35:56 -0500

    Make mRLFE examples write AE-style outputs and titles

--- analysis/mrlfe/mrlfeRunSweepExample.m
+++ analysis/mrlfe/mrlfeRunSweepExample.m
@@ -1,43 +1,97 @@
-function [sweepResults, sweepSummary] = mrlfeRunSweepExample(sweepName, branchName, varargin)
+function [sweepResults, sweepSummary, fig, outputFolder, figureFolder] = mrlfeRunSweepExample(sweepName, branchName, varargin)
 %MRLFERUNSWEEPEXAMPLE Run a maintained mRLFE sweep example.
 %
 % This helper centralizes the shared setup used by the public mRLFE sweep
 % entrypoints. Public scripts should define only the sweep and branch.
-%
-% Example:
-%   [S, T] = mrlfeRunSweepExample("viscosity", "A0Like", "AssignToBase", true);
 
 p = inputParser;
 addRequired(p, 'sweepName', @(x)ischar(x) || isstring(x));
 addRequired(p, 'branchName', @(x)ischar(x) || isstring(x));
 addParameter(p, 'AssignToBase', false, @(x)islogical(x) || isnumeric(x));
-addParameter(p, 'NewFigure', true, @(x)islogical(x) || isnumeric(x));
+addParameter(p, 'WriteOutputs', false, @(x)islogical(x) || isnumeric(x));
+addParameter(p, 'LaunchFolder', pwd, @(x)ischar(x) || isstring(x));
+addParameter(p, 'ScriptFile', '', @(x)ischar(x) || isstring(x));
 parse(p, sweepName, branchName, varargin{:});
 
 sweepName = lower(string(p.Results.sweepName));
 branchName = string(p.Results.branchName);
 
 [sweepSpec, caseInfo] = mrlfeMakeSweepSpec(sweepName);
-params = mrlfeDefaultSweepParams();
+baseParams = mrlfeDefaultSweepParams();
 options = mrlfeDefaultSweepOptions(branchName, 'EtaS', caseInfo.fixedEtaS);
 
-sweepResults = runParametricSweep(params, options, sweepSpec);
+fprintf('\nmRLFE %s sweep\n', char(sweepName));
+fprintf('Launch folder: %s\n', char(string(p.Results.LaunchFolder)));
+fprintf('%s values: %s %s\n', char(string(sweepSpec.label)), mat2str(sweepResultsDisplayValues(sweepSpec)), char(string(sweepSpec.units)));
+fprintf('Frequency range: %.3g Hz to %.3g kHz\n', baseParams.fmin, baseParams.fmax / 1e3);
+fprintf('Branch: %s\n\n', char(branchName));
 
-plotTitle = sprintf('Viscoelastic %s Cp sensitivity to %s', ...
-    char(branchName), char(caseInfo.titleParameter));
-
-plotParametricSweepCp(sweepResults, caseInfo.modelName, branchName, ...
-    'Title', plotTitle, ...
-    'NewFigure', logical(p.Results.NewFigure), ...
-    'ShowLastValidPoint', caseInfo.showLastValidPoint);
-
+sweepResults = runParametricSweep(baseParams, options, sweepSpec);
 sweepSummary = summarizeParametricSweepBranch(sweepResults, ...
     caseInfo.modelName, branchName);
+
+plotTitle = sprintf('mRLFE %s sensitivity to %s', ...
+    char(formatBranchForTitle(branchName)), char(caseInfo.titleParameter));
+
+fig = plotParametricSweepCp(sweepResults, caseInfo.modelName, branchName, ...
+    'Title', plotTitle, ...
+    'FrequencyScale', 1e3, ...
+    'FrequencyUnit', "kHz", ...
+    'StartFrequencyAtZero', true, ...
+    'ShowLastValidPoint', false);
+
+outputFolder = "";
+figureFolder = "";
+if logical(p.Results.WriteOutputs)
+    sweepMetadata = struct();
+    sweepMetadata.sweepName = sweepName;
+    sweepMetadata.branchName = branchName;
+    sweepMetadata.sweepSpec = sweepSpec;
+    sweepMetadata.referenceMu_kPa = baseParams.E / 3 / 1e3;
+    sweepMetadata.referenceEtaS_Pa_s = caseInfo.fixedEtaS;
+    sweepMetadata.referenceThickness_mm = baseParams.thickness * 1e3;
+
+    outputFolder = mrlfeWriteSweepOutputs(p.Results.LaunchFolder, ...
+        caseInfo.taskName, caseInfo.taskName, baseParams, options, ...
+        sweepMetadata, sweepResults, sweepSummary);
+
+    scriptFile = string(p.Results.ScriptFile);
+    if strlength(scriptFile) > 0
+        figureFolder = mrlfeSaveExampleFigure(fig, scriptFile, ...
+            caseInfo.taskName, caseInfo.filePrefix + "_" + branchName);
+    end
+end
 
 if logical(p.Results.AssignToBase)
     [resultName, summaryName] = mrlfeSweepWorkspaceNames(sweepName, branchName);
     assignin('base', resultName, sweepResults);
     assignin('base', summaryName, sweepSummary);
+    if strlength(string(outputFolder)) > 0
+        assignin('base', resultName + "OutputFolder", outputFolder);
+    end
+    if strlength(string(figureFolder)) > 0
+        assignin('base', resultName + "FigureFolder", figureFolder);
+    end
+end
+end
+
+function values = sweepResultsDisplayValues(sweepSpec)
+if isfield(sweepSpec, 'displayValues') && ~isempty(sweepSpec.displayValues)
+    values = sweepSpec.displayValues;
+else
+    values = sweepSpec.values ./ sweepSpec.displayScale;
+end
+end
+
+function txt = formatBranchForTitle(branchName)
+branchName = string(branchName);
+switch branchName
+    case "A0Like"
+        txt = "A0-like";
+    case "S0Like"
+        txt = "S0-like";
+    otherwise
+        txt = branchName;
 end
 end
 
@@ -46,20 +100,14 @@ sweepName = lower(string(sweepName));
 branchName = string(branchName);
 
 switch sweepName
+    case {"mu", "stiffness"}
+        prefix = "MRLFEMuSweep";
     case "viscosity"
-        prefix = "ViscositySweep";
-    case "stiffness"
-        prefix = "StiffnessSweep";
+        prefix = "MRLFEViscositySweep";
     case "thickness"
-        prefix = "ThicknessSweep";
+        prefix = "MRLFEThicknessSweep";
     otherwise
         error('Unsupported mRLFE sweepName "%s".', char(sweepName));
-end
-
-if branchName == "A0Like" && sweepName == "viscosity"
-    resultName = 'ViscositySweepResults';
-    summaryName = 'ViscositySweepSummary';
-    return;
 end
 
 resultName = char(prefix + branchName + "Results");
