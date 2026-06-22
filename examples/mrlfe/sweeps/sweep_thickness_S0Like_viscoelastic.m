9935990f2185adcefda403d2289507146f82b130
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-06-21 19:41:29 -0500

    Set mRLFE thickness S0 wrapper output paths

--- examples/mrlfe/sweeps/sweep_thickness_S0Like_viscoelastic.m
+++ examples/mrlfe/sweeps/sweep_thickness_S0Like_viscoelastic.m
@@ -1,4 +1,6 @@
 clear; clc; close all;
+launchFolder = pwd;
+scriptFile = mfilename('fullpath');
 startup
 
-[sweepResults, sweepSummary] = mrlfeRunSweepExample('thickness', 'S0Like', 'AssignToBase', true, 'WriteOutputs', true);
+[sweepResults, sweepSummary, fig, outputFolder, figureFolder] = mrlfeRunSweepExample('thickness', 'S0Like', 'AssignToBase', true, 'WriteOutputs', true, 'LaunchFolder', launchFolder, 'ScriptFile', scriptFile);
