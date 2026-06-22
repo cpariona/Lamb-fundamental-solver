9df7797c14dfbc3a40f7755b47913bcfaecda13f
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-06-22 18:50:39 -0500

    Use shared elastic material helpers in Rayleigh-Lamb

--- models/rayleigh_lamb/core/rlComputeMaterial.m
+++ models/rayleigh_lamb/core/rlComputeMaterial.m
@@ -1,40 +1,24 @@
 function material = rlComputeMaterial(params)
-% Compute E, nu, lambda, mu, CL and CT based on selected material model.
+% Compute isotropic elastic material quantities based on selected material model.
+%
+% Maintained user-facing soft-material workflows should use ShearPoisson:
+%   params.mu, params.nu, params.rho
+%
+% LameParameters is retained as an explicit internal/diagnostic alternative.
 
 rho = params.rho;
 modelType = string(params.modelType);
 
 switch modelType
-    case "YoungPoissonFixedCL"
-        E = params.E;
-        nu = params.nu;
-        CL = params.CL;
-
-        mu = E / (2 * (1 + nu));
-        lambda = E * nu / ((1 + nu) * (1 - 2 * nu));
-        CT = sqrt(mu / rho);
+    case "ShearPoisson"
+        material = elasticFromMuNu(params.mu, params.nu, rho);
 
     case "LameParameters"
-        lambda = params.lambda;
-        mu = params.mu;
-
-        E = mu * (3 * lambda + 2 * mu) / (lambda + mu);
-        nu = lambda / (2 * (lambda + mu));
-
-        CL = sqrt((lambda + 2 * mu) / rho);
-        CT = sqrt(mu / rho);
+        material = elasticFromLame(params.lambda, params.mu, rho);
 
     otherwise
-        error('Unknown material model type.');
+        error('Unknown material model type: %s.', modelType);
 end
 
-material = struct();
 material.modelType = modelType;
-material.rho = rho;
-material.E = E;
-material.nu = nu;
-material.lambda = lambda;
-material.mu = mu;
-material.CL = CL;
-material.CT = CT;
 end
