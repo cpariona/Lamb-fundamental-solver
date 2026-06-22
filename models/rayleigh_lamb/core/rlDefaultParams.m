0aa6c9af916d0932dd26d68570f07c9e44458367
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-06-22 18:50:26 -0500

    Make ShearPoisson the Rayleigh-Lamb default material model

--- models/rayleigh_lamb/core/rlDefaultParams.m
+++ models/rayleigh_lamb/core/rlDefaultParams.m
@@ -2,13 +2,19 @@ function params = rlDefaultParams()
 % Return default physical, geometry, and frequency parameters.
 
 params = struct();
-params.modelType = "YoungPoissonFixedCL";
+params.modelType = "ShearPoisson";
 params.rho = 1070;
-params.E = 475e3;
-params.nu = 0.4999;
-params.CL = 1500;
-params.lambda = 2.40e9;
 params.mu = 158e3;
+params.nu = 0.4999;
+
+% Derived values are stored for display and compatibility with older scripts.
+elastic = elasticFromMuNu(params.mu, params.nu, params.rho);
+params.E = elastic.E;
+params.lambda = elastic.lambda;
+params.K = elastic.K;
+params.CL = elastic.CL;
+params.CT = elastic.CT;
+
 params.thickness = 0.50e-3;
 params.fmin = 10;
 params.fmax = 8000;
