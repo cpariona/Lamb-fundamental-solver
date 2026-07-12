7bafdfb0bb524384fe4508d6c0b1eb420af6fade
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-07-12 02:03:24 -0500

    Route elastic A0Like through robust-start policy

--- models/mrlfe/solvers/mrlfeSolveElasticBranch.m
+++ models/mrlfe/solvers/mrlfeSolveElasticBranch.m
@@ -9,7 +9,7 @@ mrlfeParams.etaL = 0;
 mrlfeParams.useComplexLambda = false;
 
 seed = mrlfeBuildSeed(problem, configuration);
-branchSolve = mrlfeTrackBranchAdaptive(problem, seed, configuration, mrlfeParams, options);
+branchSolve = mrlfeTrackBranchRobustStart(problem, seed, configuration, mrlfeParams, options);
 branchSolve = mrlfeApplyTerminationPolicy(branchSolve, seed, configuration);
 branchSolve.productionPolicy = branchPolicyName(configuration.branch, configuration.terminationPolicy, "elastic");
 branchSolve.solverRoute = "elasticAdaptive";
