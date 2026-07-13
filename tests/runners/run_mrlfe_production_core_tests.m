f724e529579d7c0cd406ae25729c5032aca8a365
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-07-12 19:59:08 -0500

    Run numerical preset grid contract in production core suite

--- tests/runners/run_mrlfe_production_core_tests.m
+++ tests/runners/run_mrlfe_production_core_tests.m
@@ -6,6 +6,7 @@ fprintf('------------------------------------\n');
 
 test_mrlfe_production_core_contract;
 test_mrlfe_production_core_presets;
+test_mrlfe_numerical_preset_grids;
 test_mrlfe_solve_frequency_override;
 test_mrlfe_robust_start_contract;
 test_mrlfe_termination_policy;
