b362e2b267da6ac1b2ede8a82b068910939a39d3
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-07-12 23:59:40 -0500

    Run optimized fit-grid and no-auto-curve contracts

--- tests/runners/run_mrlfe_fit_public_solver_tests.m
+++ tests/runners/run_mrlfe_fit_public_solver_tests.m
@@ -4,6 +4,8 @@ startup
 fprintf('\nRunning mRLFE FitTool public-solver migration tests...\n');
 fprintf('-----------------------------------------------------\n');
 
+test_mrlfe_fit_frequency_grid_contract;
+test_fit_display_curve_no_solver_contract;
 test_mrlfe_fit_uses_public_solver;
 test_mrlfe_fit_public_solver_characterization;
 test_mrlfe_fit_public_solver_parameter_regression;
