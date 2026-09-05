d4189607ed6006fd1b2d562e1befecbcd77f0796
Carlos Pariona <43526475+cpariona@users.noreply.github.com>
2026-09-05 14:29:29 -0500

    Remove obsolete benchmark test from performance runner

--- tests/runners/run_performance_and_benchmark_tests.m
+++ tests/runners/run_performance_and_benchmark_tests.m
@@ -9,14 +9,13 @@ runTests();
 end
 
 function runTests()
-fprintf('\nRunning performance and benchmark tests...\n');
-fprintf('------------------------------------------\n');
+fprintf('\nRunning performance tests...\n');
+fprintf('----------------------------\n');
 
 test_runtime_measurement_output_schema;
 test_execution_profile_diagnostics_format;
-test_mrlfe_execution_profile_benchmark_contract;
 test_mrlfe_fit_grid_policy_performance;
 test_mrlfe_production_core_performance;
 
-fprintf('\nPerformance and benchmark tests passed.\n');
+fprintf('\nPerformance tests passed.\n');
 end
